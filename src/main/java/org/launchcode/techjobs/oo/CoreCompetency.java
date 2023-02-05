71eb1b6179e5e315182e18e77364487be97df5dd
jdawdy <jacobldawdy@gmail.com>
2023-02-05 16:59:15 -0600

    Added equals, getters, setters, and hashCode in PositionType, CoreCompetency and Location

--- src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
+++ src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
@@ -20,22 +20,21 @@ public class CoreCompetency {
 
     // Custom toString, equals, and hashCode methods:
 
-    @Override
-    public String toString() {
+    public String getValue() {
         return value;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof CoreCompetency)) return false;
-        CoreCompetency that = (CoreCompetency) o;
-        return id == that.id;
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public int getId() {
+        return id;
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(id);
+    public String toString() {
+        return value;
     }
 
     // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
